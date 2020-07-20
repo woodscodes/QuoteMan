e7080277bd19fd966095d4045697f369a85824ae
stuwo <stooberto@gmail.com>
2020-07-21 08:59:47 +0900

    Added controller for quotes (beginning to build up the api!)

---
+++ QuoteMan-b0.1.WebUI/Api/QuotesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuoteMan_b0._1.Core.Models;
+using QuoteMan_b0._1.Data.Infrastructure.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuoteMan_b0._1.WebUI.Api
+{
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        private IQuoteData _quoteData;
+
+        public QuotesController(IQuoteData quoteData)
+        {
+            _quoteData = quoteData;
+        }
+
+        [HttpGet]
+        [Route("api/{controller}/{id}")]
+        public IActionResult GetQuotes(int id)
+        {
+            var quotes = _quoteData.GetQuotesByCustomerId(id);
+
+            if (quotes != null)
+                return Ok(quotes);
+
+            return NotFound();
+        }
+    }
+}
